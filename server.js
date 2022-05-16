eb5a2ec8aac2182403c09557b663de8130abc3f5
DONNY345 <89885417+DONNY345@users.noreply.github.com>
2022-05-16 13:37:54 +1200

    env changes

--- server.js
+++ server.js
@@ -2,9 +2,12 @@ const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
 const MongoClient = require("mongodb").MongoClient;
+const dotenv = require('dotenv').config();
+const PORT = process.env.PORT || 3000
 //change password with username password. remove firstproject. app.post below will create new DB called node-demo
-const dbURL =
-  "mongodb+srv://kurtdonaldson2022:mongoDB2022@cluster0.p9wbv.mongodb.net/?retryWrites=true&w=majority";
+const dbURL = process.env.DB_URL
+  
+
 
 
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -122,6 +125,6 @@ app.put("/users", (req, res) => {
   });
 });
 
-app.listen(3000, () => {
-  console.log("LISTENING ON 3000");
+app.listen(PORT, () => {
+  console.log("LISTENING ON " + PORT + "...");
 });
